1b98a955a071c77d6f09c0b3ab7c73f874d6f4a3
Nicolas <25302138+KOOTSTHEHOOTS@users.noreply.github.com>
2021-05-25 16:20:57 +0800

    Add day header row to calendar

--- moments/src/Calendar.js
+++ moments/src/Calendar.js
@@ -1,6 +1,35 @@
+const dayNames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+];
+
+function DayHeaderRow() {
+    const row = dayNames.map((day) =>
+        <td>{day}</td>);
+
+    return (
+        <tr>
+            {row}
+        </tr>
+    );
+}
+
+/*
+    Calendar box for one month
+*/
 function Calendar() {
-    return (<>
-    </>);
+    return (
+        <>
+            <table>
+                <DayHeaderRow />
+            </table>
+        </>
+    );
 }
 
 export default Calendar;
