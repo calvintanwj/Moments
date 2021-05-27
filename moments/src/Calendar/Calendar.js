64a87d0538d6e8348d5e52dd365556cd83da7a8f
Nicolas <25302138+KOOTSTHEHOOTS@users.noreply.github.com>
2021-05-27 16:51:31 +0800

    Move Calendar Body creation into a function
    Move loop that was used to create calendar body into a function.
    Change DayData into a state that can be used by WeekRow components

--- moments/src/Calendar/Calendar.js
+++ moments/src/Calendar/Calendar.js
@@ -16,11 +16,10 @@ function Calendar(props) {
     // keep track of the day that is currently selected
     const [selectedDay, setDay] = useState(null);
     const [formEventName, setFormEventName] = useState("");
-    const dayData = [];
-
+    const [dayData, setDayData] = useState(createEmptyCalendar());
 
     /*
-        Handler for cell in calendar when clicked
+        Handler to focus cell in calendar when clicked
     */
     function dayHandler(label, row, col) {
         console.log(`${row}row, ${col}col`);
@@ -39,47 +38,54 @@ function Calendar(props) {
         Handler for button to submit details from temporary input field to make a new event
     */
     function handleSubmitEvent() {
-        console.log("Created event " + formEventName);
+        //clear form
         setFormEventName("");
     }
 
-    let day = 1;
-    let offsetCounter = 0;
-    let boxKey = 0;
+    /* 
+        Helper function to create the body of an empty calendar
+    */
+    function createEmptyCalendar() {
+        const dayData = [];
+        let day = 1;
+        let offsetCounter = 0;
+        let boxKey = 0;
 
-    // Create a 5 row by 7 col calendar body to fill with numbers 
-    for (let i = 0; i < 6; i++) {
-        const row = new Array(6);
-        for (let j = 0; j < 7; j++) {
-            const dayObject = {
-                events: [],
-                key: boxKey,
-                row: i,
-                col: j
-            }
-            if (offsetCounter < props.offset) { // offset the first day of the month
-                row[j] = {
-                    ...dayObject,
-                    label: ""
+        // Create a 5 row by 7 col calendar body to fill with numbers 
+        for (let i = 0; i < 6; i++) {
+            const row = new Array(6);
+            for (let j = 0; j < 7; j++) {
+                const dayObject = {
+                    events: [],
+                    key: boxKey,
+                    row: i,
+                    col: j
                 }
-                offsetCounter += 1;
+                if (offsetCounter < props.offset) { // offset the first day of the month
+                    row[j] = {
+                        ...dayObject,
+                        label: ""
+                    }
+                    offsetCounter += 1;
 
-            } else if (day <= props.days) { // normal calendar days
-                row[j] = {
-                    ...dayObject,
-                    label: day,
+                } else if (day <= props.days) { // normal calendar days
+                    row[j] = {
+                        ...dayObject,
+                        label: day,
+                    }
+                    day += 1;
+                } else { // Calendar month has been labelled and remainer of cells should be empty
+                    row[j] = {
+                        ...dayObject,
+                        label: ""
+                    }
                 }
-                day += 1;
-            } else { // Calendar month has been labelled and remainer of cells should be empty
-                row[j] = {
-                    ...dayObject,
-                    label: ""
-                }
+                boxKey += 1;
+                // ["Goto the barber", "Go workout"]
             }
-            boxKey += 1;
-            // ["Goto the barber", "Go workout"]
+            dayData.push(row);
         }
-        dayData.push(row);
+        return dayData;
     }
 
     return (
