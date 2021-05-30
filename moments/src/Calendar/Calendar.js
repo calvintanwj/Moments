f2fc5563b6fbd7cde40af99145314f8dd4b10cf8
Nicolas <25302138+KOOTSTHEHOOTS@users.noreply.github.com>
2021-05-30 14:09:40 +0800

    Change createEmptyCalendar into a functional component
    createEmptyCalendar is now a component called CalendarBody. Commented
    out all functionality of calendar that is now broken

--- moments/src/Calendar/Calendar.js
+++ moments/src/Calendar/Calendar.js
@@ -1,16 +1,12 @@
 import './Calendar.css';
-import WeekRow from './WeekRow.js';
 import DayHeaderRow from './DayHeaderRow';
+import CalendarBody from './CalendarBody';
 import React, { useState } from 'react';
 
 import {
-    addDays,
     addMonths,
-    getDay,
     getDate,
     format,
-    startOfMonth,
-    endOfMonth
 } from 'date-fns'
 
 
@@ -25,21 +21,21 @@ function Calendar() {
     const [selectedDay, setDay] = useState(getDate(date)); // for debugging
     const [formEventName, setFormEventName] = useState("");
 
-    // generate calendar body data - default is current calendar month
-    const [dayData, setDayData] = useState(createEmptyCalendar(date));
+    // // generate calendar body data - default is current calendar month
+    // const [dayData, setDayData] = useState(CalendarBody(date));
 
     // index of current selected day in dayData array
-    const [row, setRow] = useState(null);
-    const [col, setCol] = useState(null);
+    // const [row, setRow] = useState(null);
+    // const [col, setCol] = useState(null);
 
     /*
         Handler to focus cell in calendar when clicked
     */
-    function dayHandler(label, row, col) {
-        setRow(row);
-        setCol(col);
-        setDay(label);
-    }
+    // function dayHandler(label, row, col) {
+    //     setRow(row);
+    //     setCol(col);
+    //     setDay(label);
+    // }
 
     /*
         Handler for change in temporary input field, used to make name a new event
@@ -52,79 +48,49 @@ function Calendar() {
     /*
         Handler for button to submit details from temporary input field to make a new event
     */
-    function handleSubmitEvent() {
-        // shallow copy columns dayData
-        const newDayData = [...dayData]
-        // deep copy the dayData -> dayData is a 2-d array containig objects
-        newDayData.map((row) => { // copy row
-            return [row.map((day) => {
-                return { ...day }  // copy object
-            })]
-        });
+    // function handleSubmitEvent() {
+    //     // shallow copy columns dayData
+    //     const newDayData = [...dayData]
+    //     // deep copy the dayData -> dayData is a 2-d array containig objects
+    //     newDayData.map((row) => { // copy row
+    //         return [row.map((day) => {
+    //             return { ...day }  // copy object
+    //         })]
+    //     });
 
-        // add new event to day
-        newDayData[row][col]["events"].push(formEventName)
-        setDayData(newDayData);
+    //     // add new event to day
+    //     newDayData[row][col]["events"].push(formEventName)
+    //     setDayData(newDayData);
 
-        //clear form
-        setFormEventName("");
-    }
+    //     //clear form
+    //     setFormEventName("");
+    // }
 
 
     /*
         function to handle states when changing of calendar dates
     */
-    function changeDate(newDate) {
-        setDate(newDate);
-        setDayData(createEmptyCalendar(newDate));
-    }
+    // function changeDate(newDate) {
+    //     setDate(newDate);
+    //     setDayData(CalendarBody(newDate));
+    // }
 
     /*
         function to handle state change when going to today
     */
-    function gotoToday() {
-        changeDate(new Date());
-    }
+    //  function gotoToday() {
+    //      changeDate(new Date());
+    //  }
 
     /*
         Change the current month that is being rendered
     */
-    function changeMonth(offset) {
-        changeDate(addMonths(date, offset));
-    }
+    //  function changeMonth(offset) {
+    //      changeDate(addMonths(date, offset));
+    //  }
 
 
-    /* 
-        Helper function to create the body of an empty calendar
-    */
-    function createEmptyCalendar(calendarDate) {
-        const dayData = [];
-        let boxKey = 0;
-        const dateOfFirstDay = startOfMonth(calendarDate);
-        let currentDifference = -getDay(dateOfFirstDay);
 
-        // Create a 5 row by 7 col calendar body to fill with numbers 
-        for (let i = 0; i < 6; i++) {
-            const row = new Array(6);
-            for (let j = 0; j < 7; j++) {
-                const currDate = addDays(dateOfFirstDay, currentDifference);
-                const dayObject = {
-                    events: [],
-                    key: boxKey,
-                    row: i,
-                    col: j,
-                    date: currDate
-                }
-                row[j] = dayObject;
-
-                currentDifference += 1;
-                boxKey += 1;
-                // ["Goto the barber", "Go workout"]
-            }
-            dayData.push(row);
-        }
-        return dayData;
-    }
 
     return (
         <>
@@ -134,11 +100,14 @@ function Calendar() {
                 onChange={handleChangeFormEventName
                 }>
             </input>
-            <input type="submit" value="Create Event" onClick={handleSubmitEvent}></input>
-
-            <button onClick={() => { changeMonth(-1) }}>Previous Month</button>
+            {/* <input type="submit" value="Create Event" onClick={handleSubmitEvent}></input> */}
+            <input type="submit" value="Create Event"></input>
+            {/* <button onClick={() => { changeMonth(-1) }}>Previous Month</button>
             <button onClick={() => { changeMonth(1) }}>Next Month</button>
-            <button onClick={gotoToday}>Today</button>
+            <button onClick={gotoToday}>Today</button> */}
+            <button>Previous Month</button>
+            <button>Next Month</button>
+            <button>Today</button>
 
             <table id="Outer-Calendar">
                 <caption>{format(date, 'MMM y')}</caption>
@@ -146,9 +115,8 @@ function Calendar() {
                     <DayHeaderRow key="DayHeaderRow" />
                 </thead>
                 <tbody>
-                    {dayData.map((row, index) =>
-                        <WeekRow key={"row-" + index} days={row} dayHandler={dayHandler} />
-                    )}
+                    {/* <CalendarBody dayhandler={dayHandler} calendarDate={date} /> */}
+                    <CalendarBody calendarDate={date} />
                 </tbody>
             </table>
         </>
