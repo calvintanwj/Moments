9feb99af7b23b9cab047552d17f1ec97c1408edc
Nicolas <25302138+KOOTSTHEHOOTS@users.noreply.github.com>
2021-06-13 14:24:41 +0800

    Create sign-up form for users

--- moments/src/SignUpPage.js
+++ moments/src/SignUpPage.js
@@ -1,5 +1,24 @@
 function SignUpPage() {
-	return <h1>This is the sign-up page</h1>
+	return (
+		<div>
+			<h1>This is the sign-up page</h1>
+			<form>
+				<div>
+					<label for="username">Username:</label>
+					<input type="text" id="username" />
+				</div>
+				<div>
+					<label for="password">Password:</label>
+					<input type="password" id="password" />
+				</div>
+				<div>
+					<label for="email">Email Address:</label>
+					<input type="emai" id="emai" />
+				</div>
+				<input type="submit" value="Sign Up" />
+			</form>
+		</div>)
+
 }
 
 export default SignUpPage;
