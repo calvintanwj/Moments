4a6c49df7cd3b13b037eb6d256418c5d67e163ea
Nicolas <25302138+KOOTSTHEHOOTS@users.noreply.github.com>
2021-06-14 18:32:09 +0800

    Connect mongodb to express
    key.js is to store environment variables such as the mongodb uri which
    contain username and password credentials

--- server/app.js
+++ server/app.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose')
+const { mongoURI } = require('./key')
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function () {
+	console.log('connected to db')
+})
+
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
 	res.send('This is the homepage')
 })
+
 app.listen(port, () => {
 	console.log(`Server listening at http://localhost:${port}`)
 })
---
+++ server/key.js
@@ -0,0 +1,6 @@
+// Holds variables and secrets that should not be commited
+
+module.exports = {
+	mongoURI: 'ADD MONGODB URI HERE'
+}
+	// mongoURI: 'mongodb+srv://NodeServer:nG9xmARwkEn3YMm@cluster0.v4mwn.mongodb.net/momentsUsers?retryWrites=true&w=majority'
