81236cd3f111417f71a719f4c402646df24ee2a4
calvintanwj <calvintanwj127@gmail.com>
2021-06-25 15:44:58 +0800

    Change Cors Policy

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
-    origin: ["https://moments-flax.vercel.app/"],
+    origin: ["moments-flax.vercel.app"],
     credentials: true,
   })
 );
