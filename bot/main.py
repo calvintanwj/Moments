3d8ab9d2eb6288d9ff751228fb4b2e71265e1738
calvintanwj <calvintanwj127@gmail.com>
2021-07-14 15:49:53 +0800

    Fix dependencies

--- bot/main.py
+++ bot/main.py
@@ -24,6 +24,7 @@ scheduler = BackgroundScheduler()
 scheduler.start()
 server = Flask(__name__)
 
+
 def add_event_markup():
     markup = InlineKeyboardMarkup()
     markup.row_width = 2
@@ -406,6 +407,7 @@ class Event:
         self.end = None
         self.reminder = None
         self.message = None
+
 
 event_dict = {}
 
@@ -1106,16 +1108,20 @@ def command_default(message):
                      message.text + "\"\nMaybe try the help page at /help")
 
 
-# SERVER SIDE 
+# SERVER SIDE
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
-   bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-   return "!", 200
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
 @server.route("/")
 def webhook():
-   bot.remove_webhook()
-   bot.set_webhook(url='https://momentsorbbot.herokuapp.com/' + TOKEN)
-   return "!", 200
-if __name__ == "__main__":
-   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 7000)))
+    bot.remove_webhook()
+    bot.set_webhook(url='https://momentsorbbot.herokuapp.com/' + TOKEN)
+    return "!", 200
 
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 7000)))
